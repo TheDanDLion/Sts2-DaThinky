0606f1baf88f26ca9505aacef2e5e413ee76173d
Dan Donley <ddonley97@gmail.com>
2026-03-11 22:36:07 -0400

    added a thought bubble to the main menu to test out incorporating scenes into the build

--- Config.cs
+++ Config.cs
@@ -1,10 +1,10 @@
 using BaseLib.Config;
 
-namespace Sts2_Template;
+namespace Sts2_DaThinky;
 
 public class Config : SimpleModConfig
 {
-    // Properties must be public static with both getters and setters. SimpleModConfig currently only auto-generates
-    // UI for bool types (as tickboxes).
-    public static bool ModOption { get; set; } = true;
+	// Properties must be public static with both getters and setters. SimpleModConfig currently only auto-generates
+	// UI for bool types (as tickboxes).
+	public static bool ModOption { get; set; } = true;
 }
--- Program.cs
+++ Program.cs
@@ -1,19 +1,32 @@
+using Godot;
 using HarmonyLib;
 using MegaCrit.Sts2.Core.Modding;
+using MegaCrit.Sts2.Core.Nodes.Screens.MainMenu;
 
-namespace Sts2_Template;
+namespace Sts2_DaThinky;
 
 [ModInitializer(nameof(Initialize))]
 public class Program
 {
-    private const string ModId = "Sts2-Template";
+	private const string ModId = "Sts2-DaThinky";
 
-    public static MegaCrit.Sts2.Core.Logging.Logger Logger { get; } = new(ModId, MegaCrit.Sts2.Core.Logging.LogType.Generic);
+	public static MegaCrit.Sts2.Core.Logging.Logger Logger { get; } = new(ModId, MegaCrit.Sts2.Core.Logging.LogType.Generic);
 
-    public static void Initialize()
-    {
-        Harmony harmony = new(ModId);
+	public static void Initialize()
+	{
+		Harmony harmony = new(ModId);
+		harmony.PatchAll();
+	}
+}
 
-        harmony.PatchAll();
-    }
+[HarmonyPatch(typeof(NMainMenu), nameof(NMainMenu._Ready))]
+public class MainMenuPatch
+{
+	[HarmonyPostfix]
+	static void AddThoughtBubble(NMainMenu __instance)
+	{
+		PackedScene scene = ResourceLoader.Load<PackedScene>("res://Scenes/ThoughtBubble.tscn");
+		TextureRect bubble = scene.Instantiate<TextureRect>();
+		__instance.AddChild(bubble);
+	}
 }
